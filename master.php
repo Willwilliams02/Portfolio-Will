f7e582c94df668333111f0ab417511de6829919f
Willwilliams02 <39130683+Willwilliams02@users.noreply.github.com>
2018-05-10 10:33:54 -0500

    Update master.php

--- master.php
+++ master.php
@@ -1,18 +1,17 @@
 <?php
- 
-if ( isset( $_REQUEST ) && !empty( $_REQUEST ) ) {
- if (
- isset( $_REQUEST['phoneNumber'], $_REQUEST['carrier'], $_REQUEST['smsMessage'] ) &&
-  !empty( $_REQUEST['phoneNumber'] ) &&
-  !empty( $_REQUEST['carrier'] )
- ) {
-  $message = wordwrap( $_REQUEST['smsMessage'], 70 );
-  $to = $_REQUEST['phoneNumber'] . '@' . $_REQUEST['carrier'];
-  $result = @mail( $to, '', $message );
-  print 'Message was sent to ' . $to;
- } else {
-  print 'Not all information was submitted.';
- }
-}
- 
+   if(isset($_POST['send'])) {
+   // Prepare the email
+   $to = 'Change
+willwilliams02ww4254503@gmail.com';
+   $from='willwilliams02ww4254503@gmail.com'
+   $subject = 'Message sent from website';
+   $message = $_POST['message'];
+   // Send it
+   $sent = mail($to, $subject, $message);
+   if($sent) {
+   echo 'Your message has been sent successfully!';
+   } else {
+   echo 'Sorry, your message could not send.';
+   }
+   }
 ?>
